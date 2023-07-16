78672b4d15258aa52dd247c4e4704145935cef79
Louis Pitts <126024745+LouisPitts@users.noreply.github.com>
2023-07-16 19:11:19 -0400

    add 8ball-
    add exercise

---
+++ react8ball-colors/8ball/eightball/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+import EightBall from "./8ball";
+
+
+function App() {
+  return (
+    <div className="App">
+      <EightBall />
+    </div>
+  );
+}
+
+export default App;
